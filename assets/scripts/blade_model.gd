fe2be32a40794d353b60f8baae795284caec04ba
erik-lance <eriklance@gmail.com>
2022-07-12 21:44:09 +0800

    Made 2d compatible

---
+++ assets/scripts/blade_model.gd
@@ -0,0 +1,20 @@
+extends Node
+
+onready var butt = $Butt
+onready var segments = $Segments
+onready var tip = $Tip
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+# This will add another blade segment onto the blade
+func add_segment():
+	pass
+	
