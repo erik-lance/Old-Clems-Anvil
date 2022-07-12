3ea5e8dae414eaf6cf333883cb9c687e2c9d96bc
erik-lance <eriklance@gmail.com>
2022-07-12 23:04:32 +0800

    Blade finished with script on adding segments and quenching
    Adding a segment is adding a blade length between the butt and tip of the sword.
    
    Quenching will change the sprites to their respective quenched states.

--- assets/scripts/blade_model.gd
+++ assets/scripts/blade_model.gd
@@ -4,10 +4,12 @@ onready var butt = $Butt
 onready var segments = $Segments
 onready var tip = $Tip
 
+var blade_segments = 1
+var quenched = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
@@ -16,5 +18,42 @@ func _ready():
 
 # This will add another blade segment onto the blade
 func add_segment():
-	pass
+	blade_segments += 1
 	
+	var s = Image.new()
+	s.load("res://assets/sprites/sword segments/unquenched_segment.png")
+	var t = ImageTexture.new()
+	t.create_from_image(s)
+	
+	var rect = TextureRect.new()
+	rect.texture = t
+	
+	segments.add_child(rect)
+
+# This will change the sprites to be quenched
+func quench():
+	var but = Image.new()
+	var seg = Image.new()
+	var tip = Image.new()
+	
+	but.load("res://assets/sprites/sword segments/quenched_hilt.png")
+	seg.load("res://assets/sprites/sword segments/quenched_segment.png")
+	tip.load("res://assets/sprites/sword segments/quenched_tip.png")
+	
+	var but_t = ImageTexture.new()
+	but_t.create_from_image(but, 2)
+	
+	var seg_t = ImageTexture.new()
+	seg_t.create_from_image(seg, 2)
+	
+	var tip_t = ImageTexture.new()
+	tip_t.create_from_image(tip, 2)
+	
+	$Butt.texture = but_t
+	
+	for s in segments.get_children():
+		s.texture = seg_t
+	
+	$Tip.texture = tip_t
+	
+	pass
