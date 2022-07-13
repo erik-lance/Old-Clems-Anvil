0c899b8059e64f3cd3906cc43f5d5e5643374583
erik-lance <eriklance@gmail.com>
2022-07-13 17:01:16 +0800

    New station

---
+++ assets/scripts/station.gd
@@ -0,0 +1,21 @@
+extends Node
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+onready var pommel_stage = $Pommel
+onready var handle_stage = $Handle
+onready var hilt_stage = $Hilt
+onready var blade_stage = $Blade
+onready var heat_stage = $Heat
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
